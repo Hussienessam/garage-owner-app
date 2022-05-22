7db153b8dcffe519114bdb2b2d48a12035188641
Hussien <hussienessam18@gmail.com>
2022-05-22 21:57:01 +0200

    add template

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+
+import HelloWorld from './components/HelloWorld.vue'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+// $(document).ready(function(){
+// 	// Activate tooltip
+// 	$('[data-toggle="tooltip"]').tooltip();
+	
+// 	// Select/Deselect checkboxes
+// 	var checkbox = $('table tbody input[type="checkbox"]');
+// 	$("#selectAll").click(function(){
+// 		if(this.checked){
+// 			checkbox.each(function(){
+// 				this.checked = true;                        
+// 			});
+// 		} else{
+// 			checkbox.each(function(){
+// 				this.checked = false;                        
+// 			});
+// 		} 
+// 	});
+// 	checkbox.click(function(){
+// 		if(!this.checked){
+// 			$("#selectAll").prop("checked", false);
+// 		}
+// 	});
+// });
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
